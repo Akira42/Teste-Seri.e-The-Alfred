06cb04a20a4a5170718bd2df87ae450f837a7b19
akira42 <vitorakira42@gmail.com>
2024-08-09 00:45:51 -0300

    Upload dos assets, estilização básica e update na busca

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import {useState, useEffect} from 'react';
+import './App.scss'
 import HeroCard from './components/HeroCard'
+import logo from './images/logo/Group.png';
   
 const publicKey = 'baa801ec60e0791b83a121f2b9f3f11a';
 const charactersUrl = 'https://gateway.marvel.com/v1/public/characters';
@@ -8,53 +10,78 @@ const charactersUrl = 'https://gateway.marvel.com/v1/public/characters';
 const App = () => {
 const [heroes, setHeroes] = useState([]);
 const [searchTerm, setSearchTerm] = useState('');
+const [loading, setLoading] = useState(false);
   
 const loadCharacters = async (search) => {
-  const URL = `${charactersUrl}?limit=20&${search ? 'name=' + search + '&' : ''}apikey=${publicKey}`;
+  setLoading(true);
+  const URL = `${charactersUrl}?limit=20&orderBy=-modified&${search ? 'nameStartsWith=' + search + '&' : ''}apikey=${publicKey}`;
 
-  const response = await fetch( URL);
-  const data = await response.json();
-  
-  setHeroes(data.data.results);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setHeroes(data.data.results);
+  } catch (error) {
+    console.error("Failed to fetch characters:", error);
+  } finally {
+    setLoading(false);
+  }
 }
-  
+
 useEffect(() => {
-  loadCharacters();
+  loadCharacters(searchTerm);
 }, []);
     
 return (
-  <div>
-      <h1>Test</h1>
-  
-      <div className="search">
-        <input 
-          placeholder="Procure por heróis"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+  <main>
+      <header className="header">
+        <div className="container logo-wrapper">
+          <a className="logo">
+            <img src={logo} alt="Marvel Search heroes" />
+          </a>
 
-        <button 
-          onClick={() => loadCharacters(searchTerm)}
-        >
-          search
-        </button>
-      </div>
-  
-      {
-      heroes?.length > 0
-        ? (
-          <div className="">
+          <h1>Explore o Universo</h1>
+          <h2>Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</h2>
+        </div>
+
+        <div className="container search">
+          <form className="searchForm">
+            <input type="button" value="Search" onClick={() => loadCharacters(searchTerm)}/>
+
+            <input 
+              placeholder="Procure por heróis"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </form>
+        </div>
+      </header>
+      
+      <div className="heroesList">
+        <div className="container">
+          {loading && <div className="load"></div>}
+
+          {!loading && heroes.length === 0 && searchTerm && (
+            <div className="empty">
+              <h2>Nenhum Herói foi encontrado</h2>
+            </div>
+          )}
+
+          {/* Display heroes */}
+          <div className="heroes">
             {heroes.map((hero) => (
               <HeroCard hero={hero} key={hero.id} />
             ))}
           </div>
-        ) : (
-          <div className="empty">
-              <h2>Nenhum Herói foi encontrado</h2>
-          </div>
-        )
-      }
-    </div>
+
+        </div>
+
+        <div className="heroesListPagination">
+          
+        </div>
+      </div>
+
+      <footer className="footer"></footer>
+    </main>
   );
 }
   
--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const HeroCard = ({ hero }) => {
-    return (
-        <div className="hero">
-          <div className="">
-            <p>{hero.name}</p>
-
-            <img src={hero.thumbnail.path !== '' ? hero.thumbnail.path + '.' + hero.thumbnail.extension : 'https://via.placeholder.com/400'} alt={hero.name}/>
-          </div>
-        </div>
-    );
+  return (
+      <div className="hero">
+        <a className="">
+          <img src={hero.thumbnail.path !== '' ? hero.thumbnail.path + '.' + hero.thumbnail.extension : 'https://via.placeholder.com/400'} alt={hero.name}/>
+          
+          <h3>{hero.name}</h3>
+        </a>
+      </div>
+  );
 }
 
 export default HeroCard;
